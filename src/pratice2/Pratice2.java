f71cc80f0f156423120156cc4658725245d36c4f
connection579 <wlswn4118@naver.com>
2020-11-04 22:23:15 +0900

    강제로 형를 바꿀려면 형의 괄호를 써서 명시 함

---
+++ src/pratice2/Pratice2.java
@@ -0,0 +1,13 @@
+package pratice2;
+
+public class Pratice2 {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println((int)ch1);
+		
+		int ch2 = 67;
+		System.out.println((int)ch2);
+	}
+
+}
